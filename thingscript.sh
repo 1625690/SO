d058fa95c5fe08560b6ccc112c697df26260c133
1625690 <crystian.perafan@correounivalle.edu.co>
2018-03-21 19:11:46 -0500

    Actualización Script

--- thingscript.sh
+++ thingscript.sh
@@ -13,5 +13,5 @@ HDPORC=$(($HDDISP * 100 / $HDTOTAL))
 echo ${NUMPROCESOS}
 echo ${PORCMEM}
 echo ${PORCHD}
-curl --silent --request POST --header "X-THINGSPEAKAPIKEY:G545PDOQ13YAZF4W" --data "field1="${NUMPROCESOS}"&field2="${PORCMEM}"&field3="${PORCHD} http://api.thingspeak.com/update
+curl --silent --request POST --header "X-THINGSPEAKAPIKEY:G545PDOQ13YAZF4W" --data "field1="${NUMPROCESOS}"&field2="${PORCMEM}"&field3="${HDPORC} http://api.thingspeak.com/update
 
